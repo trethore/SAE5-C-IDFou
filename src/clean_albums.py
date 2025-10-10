65cf614ec62d2d9d76e58ca8b98aacbee4cee35c
jocooper <jocooper2018@gmail.com>
2025-10-10 11:23:30 +0200

    Added the script that remove useless columns from raw_albums.

---
+++ src/clean_albums.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+
+import pandas
+
+
+RAW_ALBUMS_CSV_PATH: str = "data/raw_albums.csv"
+
+RAW_ALBUMS_RULES: dict[str, dict[str, list[str]]] = {
+    "raw_albums": {
+        "album_id": ["notNull", "unique", "int", "notNegative"],
+        "album_title": ["notNull", "string"],
+        "album_type": ["notNull", "string"],
+        "album_tracks": ["notNull", "int", "positiveNumber"],
+        "album_date_released": ["date", "beforeNow"],
+        "album_listens": ["notNull", "int", "notNegative"],
+        "album_favorites": ["notNull", "int", "notNegative"],
+        "album_comments": ["notNull", "int", "notNegative"],
+        "album_producer": ["string"],
+        "tags": ["array", "string"],
+    }
+}
+
+
+def main() -> None:
+    """
+    Remove useless columns from raw_albums.csv and format dates to YYYY-MM-DD.
+    """
+    try:
+        raw_album_df: pandas.DataFrame = pandas.read_csv(RAW_ALBUMS_CSV_PATH)
+        columns_to_drop: list[str] = []
+        for column in raw_album_df.columns:
+            if column not in RAW_ALBUMS_RULES["raw_albums"].keys():
+                columns_to_drop.append(column)
+        clean_album_df: pandas.DataFrame = raw_album_df.drop(columns_to_drop, axis=1)
+        clean_album_df["album_date_released"] = pandas.to_datetime(
+            clean_album_df["album_date_released"], format="%m/%d/%Y"
+        )
+        clean_album_df["album_date_released"] = clean_album_df[
+            "album_date_released"
+        ].dt.strftime("%Y-%m-%d")
+        clean_album_df.to_csv("data/clean_albums.csv")
+    except FileNotFoundError:
+        print(
+            "Error: raw_albums.csv not found. Make sure to run the script from the root of the repository."
+        )
+
+
+if __name__ == "__main__":
+    main()

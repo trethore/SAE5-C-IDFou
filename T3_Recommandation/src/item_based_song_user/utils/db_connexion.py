3982e54cbbd0ffb369e61ddc9c5439e7e2fb97be
mlebout <mlebout@etudiant.univ-rennes1.fr>
2025-12-18 22:37:28 +0100

    fix(db_connexion): update database connection logic and load environment variables

--- T3_Recommandation/src/item_based_song_user/utils/db_connexion.py
+++ T3_Recommandation/src/item_based_song_user/utils/db_connexion.py
@@ -1,21 +1,23 @@
 import psycopg2
 import os
+from dotenv import load_dotenv
 
 def get_db_connection():
     """Create database connection."""
-    DB_HOST = "localhost"
-    DB_PORT = os.getenv("PGDB_PORT", "5432")
-    DB_NAME = os.getenv("DB_NAME", "sae5idfou")
-    DB_USER = os.getenv("DB_USER", "idfou")
-    DB_PASSWORD = os.getenv("DB_ROOT_PASSWORD")
-    
+    # DB_HOST = "localhost"
+    # DB_PORT = os.getenv("PGDB_PORT", "5432")
+    # DB_NAME = os.getenv("DB_NAME", "sae5idfou")
+    # DB_USER = os.getenv("DB_USER", "idfou")
+    # DB_PASSWORD = os.getenv("DB_ROOT_PASSWORD")
+    load_dotenv()
+
     try:
         return psycopg2.connect(
-            host=DB_HOST,
-            port=DB_PORT,
-            dbname=DB_NAME,
-            user=DB_USER,
-            password=DB_PASSWORD
+            host="localhost",
+            port=os.environ["PGDB_PORT"],
+            dbname=os.environ["DB_NAME"],
+            user=os.environ["DB_USER"],
+            password=os.environ["DB_ROOT_PASSWORD"],
         )
     except Exception as e:
         print(f"Error connecting to database: {e}")
